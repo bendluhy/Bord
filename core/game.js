a4a4604704e7503c517f4b6903a46f133c7abaf9
bendluhy <bendluhy@gmail.com>
2023-12-04 23:16:48 -0500

    Added core pixi js

--- core/game.js
+++ core/game.js
@@ -1,14 +1,11 @@
-// Create the application helper and add its render target to the page
-let app = new PIXI.Application({ width: 640, height: 360 });
-document.body.appendChild(app.view);
-
-// Create the sprite and add it to the stage
-let sprite = PIXI.Sprite.from('IMG_3610.png');
-app.stage.addChild(sprite);
-
-// Add a ticker callback to move the sprite back and forth
-let elapsed = 0.0;
-app.ticker.add((delta) => {
-  elapsed += delta;
-  sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+// Create the PixiJS application
+const app = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: 0x000000, // Set background to black
+  autoResize: true, // Automatically resize the renderer
 });
+
+
+// Add the renderer view to the DOM
+document.body.appendChild(app.view);
